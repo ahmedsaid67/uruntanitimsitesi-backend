71ecea77856ca5c4f384a594d8194e9bb2f11357
ahmedsaid67 <96987324+ahmedsaid67@users.noreply.github.com>
2024-06-29 19:59:13 +0300

    Update apps.py

--- myproject/myapp/apps.py
+++ myproject/myapp/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
+
+    def ready(self):
+        import myapp.signals
